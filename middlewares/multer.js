53b655fa3d0b0bca804c026e16ab2c02c0391c4a
tts2k <tranthaison2000@gmail.com>
2022-11-03 13:13:07 -0400

    multer: fix allowedMimeTypes

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -1,6 +1,7 @@
 const httpStatus = require('http-status');
 const multer = require('multer');
 const ApiError = require('../utils/ApiError');
+const { allowedMimeTypes } = require('../constants');
 
 const storage = multer({
   storage: multer.memoryStorage({
@@ -9,7 +10,7 @@ const storage = multer({
         return next(ApiError(httpStatus.BAD_REQUEST, "Invalid field name"));
       }
 
-      if (file.fieldname === "image" && !AllowedMimeTypes.includes(file.mimetype)) {
+      if (file.fieldname === "image" && !allowedMimeTypes.includes(file.mimetype)) {
           return next(ApiError(httpStatus.BAD_REQUEST, "Not an image"));
       }
     }
