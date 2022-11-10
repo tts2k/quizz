adddbfcaebbc09bab3bb90ae446a4182e0f5f902
tts2k <tranthaison2000@gmail.com>
2022-11-10 09:15:15 -0500

    ui: init ui and add home screen

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Home from './pages/Home';
+import Login from './pages/auth/Login';
+import Register from './pages/auth/Register';
+import NavBar from './components/NavBar/NavBar';
+
+export default function App() {
+  return (
+    <>
+      <NavBar />
+      <Box style={{ marginTop: "130px" }}></Box>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/register" element={<Register />}/>
+      </Routes>
+    </>
+  )
+}
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import QuestionList from '../components/QuestionList/QuestionList';
+
+export default function Home() {
+  return (
+    <Box>
+      <QuestionList />
+    </Box>
+  )
+}
